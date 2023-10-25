2d4bf9473ce7bbc34828130c80d1eddc859f1160
ptdrpg <pagielamizael@gmail.com>
2023-10-25 15:17:29 +0300

    commit 3

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import Nav from './component/layout/nav/Nav';
 import Login from './page/Login';
 import Footer from './component/layout/Footer/Footer';
 import SIgnup from './page/SIgnup';
+import { useState } from 'react';
+import { secureHeapUsed } from 'crypto';
 
 function App() {
+  const [connected, setConnected] = useState<boolean>(true)
   return (
     <BrowserRouter>
-      <Nav />
+      <Nav connected={connected} setConnected={setConnected} />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
--- src/component/layout/nav/Nav.tsx
+++ src/component/layout/nav/Nav.tsx
@@ -4,13 +4,29 @@ import Linkbutton from '../../general/bouton/Linkbutton'
 import Logo from '../../icon/Logo'
 import Searchinput from '../../general/input/Searchinput'
 import Linktext from '../../text/Linktext'
+import Acount from '../../icon/Acount'
+import Notification from '../../icon/Notification'
+import { useLayoutEffect } from 'react'
 
-type Props = {}
+type Props = {
+  connected: boolean,
+  setConnected: React.Dispatch<React.SetStateAction<boolean>>
+}
 type item = { label: string, link: string }
 
 const items:item[]=[{label: 'Home', link: '/'},{label: 'Categories', link: '/category'},{label: 'Meal Planners', link: '/planner'},{label: 'Courses', link: '/courses'},{label: 'About us', link: '/about'},{label: 'Contact', link: '/contact'}]
 
-const Nav = (props: Props) => {
+
+const Nav = ({ connected, setConnected }: Props) => {
+  useLayoutEffect(() => {
+    const getToken = localStorage.getItem('token');
+    if (getToken) {
+      setConnected(false);
+    }
+  })
+  const logout = () => {
+    localStorage.clear();
+  }
   return (
     <div className='navbar'>
       <Logo />
@@ -19,11 +35,20 @@ const Nav = (props: Props) => {
           items.map((value) => <li>{<Linktext label={value.label} link={value.link} className='navItems' />}</li> )
         }
       </ul>
-      <div className='flex'>
+      {
+        connected && <div className='flex'>
         <Searchinput />
         <Linkbutton label='Login' link='/login' className='login' />
         <Linkbutton label='Signup' link='/signup' className='signup' />
       </div>
+      }
+      {
+        !connected && <div className='connectedbutton'>
+            <button  ><Acount /></button>
+            <button  ><Notification /></button>
+            <button className='logButton' onClick={logout} >Logout</button>
+          </div>
+      }
     </div>
   )
 }
